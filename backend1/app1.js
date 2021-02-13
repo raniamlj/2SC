467fad77c94f95bb38b0a564e4ace20dd13ce0e4
raniamlj <raniamlj@gmail.com>
2021-02-13 20:33:48 +0100

    second

--- backend1/app1.js
+++ backend1/app1.js
@@ -1,11 +1,61 @@
-const express = require('express')
+const express = require('express');
+const app1 = express();
+const fetch   = require('node-fetch');
+
+app1.use((req, res) => {
+    fetch("http://localhost:4002/data")
+    .then(res => res.json())
+    .then(data => {
+        res.send({ data });})
+
+   //res.json({ message: 'Votre requête a bien été reçue !' }); 
+
+});
+
+module.exports = app1;
+
+/*fetch("http://localhost:4002/data")
+.then((response)=> response.json())
+.then((data)=>console.log(data))
+.catch(error => console.log("error"))*/
+
+
+
+
+
+/*var express = require('express');
+var app1 = express.Router();
+var request = require('request');
+app1.get('/', function(req, res, next) {
+  request({
+    uri: 'https://localhost:4002/data/.json'
+  }).pipe(res);
+})*/
+
+
+
+/*const express = require('express')
 const app1 = express()
-const parkings = require('http://localhost:4002/data')
+const parkings = require('https://localhost:4002/data')
 
 app1.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
 })
 
-app1.listen(4003, () => {
+app1.listen(3000, () => {
     console.log("Serveur à l'écoute")
-})
+})*/
+
+/*const fetch = require('node-fetch');
+
+let url = "http://localhost:4002/data";
+
+let settings = { method: "Get" };
+
+fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+        // do something with JSON
+    });*/
+
+module.exports = app1;
--- backend2/app2.js
+++ backend2/app2.js
@@ -13,6 +13,12 @@ module.exports = app2;*/
 const app2 = express();
 const data = require('./data.json');
 
+app2.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app2.get('/data', (req,res) => {
     res.status(200).json(data)
